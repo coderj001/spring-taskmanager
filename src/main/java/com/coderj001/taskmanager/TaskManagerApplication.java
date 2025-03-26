72c8beeaa58a22646e04e5d56dc7dd2a65af34a8
Raju Ghorai <raju.ghorai@everest.enginering>
2025-03-26 22:05:24 +0530

    init java project

---
+++ src/main/java/com/coderj001/taskmanager/TaskManagerApplication.java
@@ -0,0 +1,14 @@
+package com.coderj001.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class TaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerApplication.class, args);
+	}
+
+}
